50b8392ea715f8a5edff8dd9e19c9f95437730e7
Chelz Chan <n10254854@qut.edu.au>
2024-03-06 18:23:58 +1000

    Implemented list-operations test

--- src/IAuthenticationService.java
+++ src/IAuthenticationService.java
@@ -2,3 +2,6 @@ public interface IAuthenticationService {
     User signUp(String username, String password);
     User logIn(String username, String password);
 }
+
+// completed implementation of list-operations test
+
